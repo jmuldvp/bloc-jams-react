7a5e1f993e1be869f95fe2e8b9c9a334fab4a11d
jmuldvp <jmul.dvp@gmail.com>
2018-04-03 23:00:25 -0600

    CP6 Album completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -4,27 +4,37 @@ import albumData from './../data/albums';
 class Album extends Component {
   constructor(props) {
     super(props);
-    // console.log(this.props.match.params.slug)
+    // console.log(this.props.albums)
     const album = albumData.find( album => {
       return album.slug === this.props.match.params.slug
     });
-
+    // console.log(this.props.match.params.slug)
     this.state = {
-      album: album
+      album: album || {},
     };
   }
   render() {
+    // console.log(this.state.album.title)
     return(
       <section className="album">
-        {this.props.match.params.slug} Album will go here
+        {this.props.match.params.slug}
         <section id="album-info">
-          <img id="album-cover-art" alt="" src={this.state.album.albumCover} />
+          <img id="album-cover-art" alt="" src={this.state.album.albumCover || ''} />
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
             <div id="release-info">{this.state.album.releaseInfo}</div>
           </div>
         </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+          </tbody>
+        </table>
       </section>
     )
   }
--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ ReactDOM.render(
    <BrowserRouter>
      <App />
    </BrowserRouter>
-   ,document.getElementById('root'));
+   , document.getElementById('root'));
 // ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
